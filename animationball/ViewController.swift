ba5d0fcc0890cde3be2fe21029e4fbb5c5c59145
duthli <hung1996latoi2@gmail.com>
2016-08-23 11:40:50 +0700

    lan dau

--- animationball/ViewController.swift
+++ animationball/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ball = UIImageView()
+    var rad = CGFloat()
+    var ballRadious = CGFloat()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let time = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector (rollBall), userInfo: nil, repeats: true)
+        addBall()
+        //rollBall()
+        moveBall()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func addBall(){
+    let ViewSize = self.view.bounds.size
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ballRadious = 32.0
+        ball.center = CGPointMake(ballRadious, ViewSize.height * 0.5)
+        self.view.addSubview(ball)
+    }
+    var x = CGFloat()
+    func rollBall(){
+        let deltaAngle: CGFloat = 0.1
+        rad = rad + deltaAngle * x
+        ball.transform = CGAffineTransformMakeRotation(rad)
+        //ball.center = CGPointMake(ball.center.x + (ballRadious * deltaAngle) , self.view.bounds.size.height * 0.5)
+    }
+    func moveBall(){
+        UIView.animateWithDuration(4, animations: {
+            self.ball.center = CGPointMake(self.view.bounds.size.width-32.0, self.yoffset + self.ballRadious)
+                self.x = 1
+            self.Random()
+            })
+        {(finished) in
+            
+            UIView.animateWithDuration(4, animations: {
+                self.ball.center = CGPointMake(32.0, self.yoffset)
+                self.x = -1
+                })
+            {(finished) in
+                self.x = 1
+                self.moveBall()
+            }
+        }
+    }
+//    func moveBalldaplai(){
+//        UIView.animateWithDuration(4, animations: {
+//            self.ball.center = CGPointMake(self.view.bounds.size.width-50, self.view.bounds.size.height * 0.5)
+//            self.x = 1
+//            })
+//        {(finished) in
+//            
+//            UIView.animateWithDuration(4, animations: {
+//                self.ball.center = CGPointMake(50, self.view.bounds.size.height * 0.5)
+//                self.x = -1
+//                })
+//            {(finished) in
+//                self.x = 1
+//                self.moveBall()
+//            }
+//        }
+//    }
+    var yoffset = CGFloat()
+    func Random(){
+        let yheight = self.view.bounds.size.height - 50
+        yoffset = CGFloat(arc4random_uniform(UInt32(yheight)))
+    }
 }
 
